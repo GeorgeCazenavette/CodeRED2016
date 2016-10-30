de48960bbacfa57861f5aa6a6f5fcfd5fcfc6708
mplouque <mplouque@gmail.com>
2016-10-30 00:03:55 -0500

    Update EmailScript.py

--- EmailScript.py
+++ EmailScript.py
@@ -69,8 +69,8 @@ quakeMessageSent = False
 timeFireSent = time.time()
 timeQuakeSent = time.time()
 while (True):
-	print ("FIRE:{}\tQUAKE{}:{}".format(GPIO.input(FIREPIN), GPIO.input(QUAKEPIN))
-	print
+	print ("FIRE:{}\tQUAKE:{}".format(GPIO.input(FIREPIN), GPIO.input(QUAKEPIN))
+	       
 	if (GPIO.input(FIREPIN) and fireMessageSent == False):
 		message = "Fire Detected! Call 911"
 		sendSMS(message)
