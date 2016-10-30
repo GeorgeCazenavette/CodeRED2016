dc1f7a76171df9d39b55e8331d7c6fd7dda68b36
George Cazenavette <gjc006@latech.edu>
2016-10-30 11:14:26 -0500

    memes

--- code/SafetyDevice/SafetyDevice.ino
+++ code/SafetyDevice/SafetyDevice.ino
@@ -1,87 +1,201 @@
 #include <math.h> 
 
 // Digital Pins
-#define FIRE_PIN 3
-#define ALARM_PIN 5
+#define RED_PIN 5
+#define GREEN_PIN 3
+#define BLUE_PIN 6
+#define FIRE_PIN 4
+#define REMOTE_PIN 8
+#define FIRE_MSG_PIN 7
+//21 on RPi
+#define ALARM_PIN 2
+#define QUAKE_PIN 12
+#define QUAKE_MSG_PIN 9
+//20 on RPi
+#define WATER_MSG_PIN 10
+//16 on RPi
+#define TEMP_MSG_PIN 11
+//12 on RPi
+
 // Analog Pins
 #define TEMP_PIN 0
-#define WATER_PIN 1
+#define WATER_PIN 5
 
-char degree = 167;
+#define CRITICAL_TEMP 76
+
+// Variables
 int temp;
 boolean fire;
-int water;
+boolean water;
 String toPrint = "";
 unsigned long lastUpdated = 0;
-
+boolean quake;
+boolean remote;
+boolean danger = false;
+boolean paused = false;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(3, INPUT);
+  // Initialize Pins for respective functions
+  pinMode(FIRE_PIN, INPUT);
   pinMode(ALARM_PIN, OUTPUT);
+  pinMode(FIRE_MSG_PIN, OUTPUT);
+  pinMode(QUAKE_MSG_PIN, OUTPUT);
+  pinMode(WATER_MSG_PIN, OUTPUT);
+  pinMode(TEMP_MSG_PIN, OUTPUT);
+  pinMode(QUAKE_PIN, INPUT);
+  pinMode(REMOTE_PIN, INPUT);
+
+  // Begin serial communication with a BAUD rate of 9600
   Serial.begin(9600);
+
+  // Write to LCD Screen
   Serial.write(22);
   Serial.write(12);
   Serial.write(17);
   Serial.write(128);
-  Serial.print("Temp: ");
+  Serial.print("Temp:  ");
   Serial.write(148);
-  Serial.print("Status: ");
+  Serial.print("Fire:  ");
   Serial.write(168);
   Serial.print("Water: ");
+  Serial.write(188);
+  Serial.print("Quake: ");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  // Standby loop 
+  StandbyCheck();
+
+  // Clear toPrint string
   toPrint = "";
-  temp = analogRead(TEMP_PIN);
+
+  // Read values from sensors
+  temp = Thermister(analogRead(TEMP_PIN));
   fire = digitalRead(FIRE_PIN);
-  water = analogRead(WATER_PIN);
-  /*toPrint += "Temperature: ";
-  toPrint += temp;
-  toPrint += "\tFire: ";
-  toPrint += (fire) ? ("FIRE") : ("NO Fire :(");*/
-  if (millis() - lastUpdated >= 250 | millis() < lastUpdated | fire)
+  water = analogRead(WATER_PIN) > 50;
+  quake = digitalRead(QUAKE_PIN);
+  remote = digitalRead(REMOTE_PIN);
+
+  // If something has changed or 250 milliseconds have passed, then update LCD Screen
+  if (millis() - lastUpdated >= 250 | millis() < lastUpdated | quake | fire | water | temp > CRITICAL_TEMP)
   {
-    Serial.write(134);
+    Serial.write(136);
     Serial.print("    ");
-    Serial.write(134);
-    Serial.print(temp);
+    Serial.write(136);
+    Serial.print((temp > CRITICAL_TEMP) ? (String(temp) + "F CRITICAL") : (String(temp) + "F         "));
     Serial.write(156);
-    Serial.print((fire) ? ("FIRE       ") : ("NO Fire :("));
-    Serial.write(175);
-    Serial.print("    ");
-    Serial.write(175);
-    Serial.print(water);
+    Serial.print((fire) ? ("DETECTED") : ("Normal  "));
+    Serial.write(176); 
+    Serial.print((water) ? ("DETECTED") : ("Normal  "));
+    Serial.write(196);
+    Serial.print((quake) ? ("DETECTED") : ("Normal  "));
     lastUpdated = millis();
   }
-    //Serial.println(toPrint);
+
+  // Set of loops to raise Alarm() if needed
   if (fire)
   {
+    digitalWrite(FIRE_MSG_PIN, HIGH);
     Alarm();
+  }
+  else
+  {
+    digitalWrite(FIRE_MSG_PIN, LOW);
+  }
+  if (quake)
+  {
+    digitalWrite(QUAKE_MSG_PIN, HIGH);
+    Alarm();
+  }
+  else
+  {
+    digitalWrite(QUAKE_MSG_PIN, LOW);
+  }
+  if (water)
+  {
+    digitalWrite(WATER_MSG_PIN, HIGH);
+    Alarm();
+  }
+  else
+  {
+    digitalWrite(WATER_MSG_PIN, LOW);
+  }
+  if (temp > CRITICAL_TEMP)
+  {
+    digitalWrite(TEMP_MSG_PIN, HIGH);
+    Alarm();
+  }
+  else
+  {
+    digitalWrite(TEMP_MSG_PIN, LOW);
+  }
+  // ************************* NEED TO SETUP WATER MSG PIN ***************************************
+
+  // Used to prevent flickering of RGB LED
+  if (!danger)
+  {
+    analogWrite(RED_PIN, 0);
+    analogWrite(BLUE_PIN, 0);
+    analogWrite(GREEN_PIN, 255);
+  }
+  
+  danger = false;
+  
+  
+}
+
+void StandbyCheck()
+{
+  // Set of loops to keep the arduino in a non-reactive state until released from standby
+  if (digitalRead(REMOTE_PIN))
+  {
+    analogWrite(RED_PIN, 0);
+    analogWrite(BLUE_PIN, 255);
+    analogWrite(GREEN_PIN, 0);
+    while (digitalRead(REMOTE_PIN))
+    {
+      ;
+    }
+    while (!digitalRead(REMOTE_PIN))
+    {
+      ;
+    }
+    while (digitalRead(REMOTE_PIN))
+    {
+      ;
+    }
+    analogWrite(RED_PIN, 0);
+    analogWrite(BLUE_PIN, 0);
+    analogWrite(GREEN_PIN, 255);
   }
 }
 
 
 
-
-
+// Function to translate rawADC data into Farenheit
 double Thermister(int RawADC) { 
   double Temp; 
-  Temp = log(((10240000/RawADC) - 10000)); 
+  Temp = log(((10240000/(1023-RawADC)) - 10000)); 
   Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp ))* Temp ); 
-  Temp = Temp - 273.15;            // Convert Kelvin to Celcius 
-  return Temp; 
+  Temp = (Temp - 273.15);            // Convert Kelvin to Celcius 
+  return Temp + 46; 
 } 
 
+// Method to sound Alarm (piezo speaker) and set status LED to RED
 void Alarm()
 {
+  danger = true;
+  analogWrite(RED_PIN, 255);
+  analogWrite(BLUE_PIN, 0);
+  analogWrite(GREEN_PIN, 0);
   for(float i=333;i<=1500;i=i+.5)
-    {
-      makeSound(ALARM_PIN,i);
-    }
+  {
+    makeSound(ALARM_PIN,i);
+  }
 }
 
+// Method to make piezo sound
 void makeSound(int pin, float freq)
 {
   digitalWrite(pin,HIGH);
@@ -89,5 +203,3 @@ void makeSound(int pin, float freq)
   digitalWrite(pin,LOW);
   delayMicroseconds(int(1/freq/2*100000));
 }
-
-
