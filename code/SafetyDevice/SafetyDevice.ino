11b0d7cab47ec4017a0f843a14d3de464532b649
George Cazenavette <gjc006@latech.edu>
2016-10-29 22:07:34 -0500

    mems

--- code/SafetyDevice/SafetyDevice.ino
+++ code/SafetyDevice/SafetyDevice.ino
@@ -5,17 +5,30 @@
 #define ALARM_PIN 5
 // Analog Pins
 #define TEMP_PIN 0
+#define WATER_PIN 1
 
 char degree = 167;
 int temp;
 boolean fire;
+int water;
 String toPrint = "";
+unsigned long lastUpdated = 0;
+
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(3, INPUT);
   pinMode(ALARM_PIN, OUTPUT);
   Serial.begin(9600);
+  Serial.write(22);
+  Serial.write(12);
+  Serial.write(17);
+  Serial.write(128);
+  Serial.print("Temp: ");
+  Serial.write(148);
+  Serial.print("Status: ");
+  Serial.write(168);
+  Serial.print("Water: ");
 }
 
 void loop() {
@@ -23,11 +36,26 @@ void loop() {
   toPrint = "";
   temp = analogRead(TEMP_PIN);
   fire = digitalRead(FIRE_PIN);
-  toPrint += "Temperature: ";
+  water = analogRead(WATER_PIN);
+  /*toPrint += "Temperature: ";
   toPrint += temp;
   toPrint += "\tFire: ";
-  toPrint += (fire) ? ("FIRE") : ("NO Fire :(");
-  Serial.println(toPrint);
+  toPrint += (fire) ? ("FIRE") : ("NO Fire :(");*/
+  if (millis() - lastUpdated >= 250 | millis() < lastUpdated | fire)
+  {
+    Serial.write(134);
+    Serial.print("    ");
+    Serial.write(134);
+    Serial.print(temp);
+    Serial.write(156);
+    Serial.print((fire) ? ("FIRE       ") : ("NO Fire :("));
+    Serial.write(175);
+    Serial.print("    ");
+    Serial.write(175);
+    Serial.print(water);
+    lastUpdated = millis();
+  }
+    //Serial.println(toPrint);
   if (fire)
   {
     Alarm();
@@ -48,13 +76,10 @@ double Thermister(int RawADC) {
 
 void Alarm()
 {
-  for(int i = 0; i == 10000000; i++)
-  {
-    digitalWrite(ALARM_PIN, HIGH);
-    delayMicroseconds(200);
-    digitalWrite(ALARM_PIN, LOW);
-    delayMicroseconds(200);
-  }
+  for(float i=333;i<=1500;i=i+.5)
+    {
+      makeSound(ALARM_PIN,i);
+    }
 }
 
 void makeSound(int pin, float freq)
@@ -65,13 +90,4 @@ void makeSound(int pin, float freq)
   delayMicroseconds(int(1/freq/2*100000));
 }
 
-void makeSound(int pin, float freq, float dur)
-{
-  for(float t=1; t<=dur/(1/freq);t++)
-  {
-    digitalWrite(pin,HIGH);
-    delayMicroseconds(int(1/freq/2*100000));
-    digitalWrite(pin,LOW);
-    delayMicroseconds(int(1/freq/2*100000));
-  }
-}
+
